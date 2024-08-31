85b5b977e4f691cf6d13d3e81c73b9523023efdf
Raj <Pranav.Raj@motherson.com>
2024-08-31 14:53:58 +0200

    Added sqlite strorage of temperature

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify
-import os, re
+import os, re, sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,6 +10,17 @@ def fetch_cpu_temp():
 	# print (f"Current temperature is: {temperature} ºC")
 	return temperature
 
+def store_temperature(temp):
+	conn = sqlite3.connect('temperature.db')
+	c = conn.cursor()
+	c.execute('''CREATE TABLE IF NOT EXISTS temperatures
+                 (id INTEGER PRIMARY KEY, timestamp TEXT, temp REAL)''')
+	c.execute("INSERT INTO temperatures (temp, timestamp) VALUES (?, ?)", (datetime.now() , temp))
+
+	conn.commit()
+	conn.close()
+
+
 @app.route("/")
 def home():
 	# cpu_temperature = float(fetch_cpu_temp())
@@ -17,6 +29,7 @@ def home():
 @app.route('/temperature')
 def temperature():
 	cpu_temperature =  fetch_cpu_temp()
+	store_temperature(cpu_temperature)  # Store temperature in DB
 	return jsonify(temperature=cpu_temperature)
 
 
